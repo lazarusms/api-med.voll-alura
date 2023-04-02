ea38cb620539794003540edf4b6b0da48ff4cdb8
Mathaus Lazarus <lazarusmathaus@gmail.com>
2023-04-01 22:28:27 -0300

    learning about the put method

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,11 +2,9 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedicos;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,7 +26,14 @@ public class MedicoController {
 
     }
     @GetMapping
-    public List<DadosListagemMedicos> listar(Pageable paginacao){
-        return repository.findAll(paginacao).stream().map(DadosListagemMedicos::new).toList();
+    public Page<DadosListagemMedicos> listar(Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedicos::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes (dados);
     }
 }
